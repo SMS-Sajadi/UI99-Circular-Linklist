b658c6be9b25d73049a213a1ac42b6fd68122361
Soroush <soroush.s2017@gmail.com>
2021-04-30 17:04:30 +0430

    Adding PUSH Function

--- Circular Linklist.cpp
+++ Circular Linklist.cpp
@@ -37,29 +37,61 @@ class Circular_linked_list{
 		int size;
 		node* current;
 	public:
-	Circular_linked_list()
-	{
-		this->current = nullptr;
-		this->size = 0;
-	}
-	~Circular_linked_list()
-	{
-		node* temp = this->current;
-		for (int i = 0; i < this->size; i++)
+		Circular_linked_list()
 		{
-			current = current->get_next();
-			delete temp;
-			temp = current;
+			this->current = nullptr;
+			this->size = 0;
 		}
-		this->current = nullptr;
-		this->size = 0;
-	}
+		~Circular_linked_list()
+		{
+			node* temp = this->current;
+			for (int i = 0; i < this->size; i++)
+			{
+				current = current->get_next();
+				delete temp;
+				temp = current;
+			}
+			this->current = nullptr;
+			this->size = 0;
+		}
 		//add getter and setter functions here
 		/*
 		
 			here
 		*/
-		void push();
+		void push(int data)
+		{
+			if (size == 0)
+			{
+				node* newnode = new node;
+				newnode->set_data(data);
+				newnode->set_next(nullptr);
+				newnode->set_prev(nullptr);
+				current = newnode;
+				size++;
+				return;
+			}
+			if (size == 1)
+			{
+				node* newnode = new node;
+				newnode->set_data(data);
+				newnode->set_next(current);
+				newnode->set_prev(current);
+				current->set_next(newnode);
+				current->set_prev(newnode);
+				current = newnode;
+				size++;
+				return;
+			}
+			node* newnode = new node;
+			newnode->set_data(data);
+			newnode->set_prev(current);
+			newnode->set_next(current->get_next());
+			current->get_next()->set_prev(newnode);
+			current->set_next(newnode);
+			current = newnode;
+			size++;
+		}
 		void pop();
 		void pop(int data);
 		void push_after(int new_data,int pa_data);
